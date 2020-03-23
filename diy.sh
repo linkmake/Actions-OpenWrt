1e501eb75e0f6fda74015f3feb26abf539975f15
linkmake <48818647+linkmake@users.noreply.github.com>
2020-03-23 20:12:37 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,8 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+cd openwrt
+cd package
+mkdir openwrt-packages
+cd openwrt-packages
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
